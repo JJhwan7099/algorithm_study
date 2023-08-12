a51d3bed88cd2ad66e5296965cdffae5386aebdc
JJhwan7099 <105360636+JJhwan7099@users.noreply.github.com>
2023-08-12 23:11:19 +0900

    [Gold V] Title: Number Matrix, Time: 136 ms, Memory: 3060 KB -BaekjoonHub

--- 백준/Gold/6881. Number Matrix/Number Matrix.cc
+++ 백준/Gold/6881. Number Matrix/Number Matrix.cc
@@ -1,36 +1,37 @@
 #include <iostream>
+#include <algorithm>
 #include <queue>
 #define FOR(i,N) for(int i=0;i<=N;i++)
 using namespace std;
-int N, M;
+int N,M;
 int graph[102][102];
 int visit[102][102];
 bool check = false;
 struct three
 {
-    int num1, num2, num3;
+    int num1,num2,num3;
 };
-vector<three> v1;
 void search(int x, int y, three t)
 {
-    if (x == M)
+    if(x == M)
     {
         check = true;
         return;
     }
     int a = graph[x][y];
-    int b[4] = { 0 , 1, -1, 0 };
-    int c[4] = { 1 , 0, 0, -1 };
-    if (a == t.num1 || a == t.num2 || a == t.num3)
+    int b[4] = {0 , 1, -1, 0};
+    int c[4] = {1 , 0, 0, -1};
+    if(a==t.num1||a==t.num2||a==t.num3)
     {
-        for (int i = 0; i < 4; i++)
+        
+        for(int i = 0; i < 4 ; i++)
         {
-            if (graph[x + b[i]][y + c[i]] == t.num1 || graph[x + b[i]][y + c[i]] == t.num2 || graph[x + b[i]][y + c[i]] == t.num3)
+            if((graph[x+b[i]][y+c[i]]==t.num1 || graph[x+b[i]][y+c[i]]==t.num2 || graph[x+b[i]][y+c[i]] == t.num3))
             {
-                if (visit[x + b[i]][y + c[i]] != 1)
+                if(visit[x+b[i]][y+c[i]]!=1)
                 {
-                    visit[x + b[i]][y + c[i]] = 1;
-                    search(x + b[i], y + c[i], t);
+                    visit[x+b[i]][y+c[i]] = 1;
+                    search(x+b[i],y+c[i],t);
                 }
             }
         }
@@ -39,38 +40,36 @@ void search(int x, int y, three t)
 int main()
 {
     cin >> N >> M;
-    FOR(i, M + 1)FOR(j, N + 1)
+    FOR(i,M+1)FOR(j,N+1) 
     {
-        if (i == 0 || i == M + 1)
+        if(i==0 || i==M+1)
         {
             graph[i][j] = -1;
             continue;
         }
-        else if (j == 0 || j == N + 1)
+        else if(j==0 || j==N+1)
         {
             graph[i][j] = -1;
             continue;
         }
         else cin >> graph[i][j];
     }
-    for (int i = 0; i <= 9; i++)for (int j = 0; j <= 9; j++)for (int k = 0;k <= 9;k++)
+    for(int i=0; i<=9; i++)for(int j=0; j<=9; j++)for(int k=0;k<=9;k++)
     {
         three t;
         t.num1 = i; t.num2 = j; t.num3 = k;
-        for (int l = 1; l <= N; l++)
+        for(int l=1; l<=N; l++)
         {
-            search(1, l, t);
-            if (check)
+            search(1,l,t);
+            if(check == true)
             {
-                cout << t.num1 << " " << t.num2 << " " << t.num3;
+                cout << t.num1 << " " << t.num2 << " " << t.num3 << endl;
                 return 0;
             }
             check = false;
-            for (int x = 0; x < 102; x++) {
-                for (int y = 0; y < 102; y++) {
-                    visit[x][y] = 0;
-                }
-            }
+            // cout << t.num1 << " " << t.num2 << " " << t.num3 << endl;
+            // FOR(i,N){FOR(j,N) cout << visit[i][j]; cout << endl;}
+            fill(&visit[0][0],&visit[M][N],0);
         }
     }
     cout << "-1 -1 -1";
