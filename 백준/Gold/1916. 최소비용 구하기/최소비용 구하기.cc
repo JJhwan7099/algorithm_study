526586817d77d6a8a2cfec578c03850a6009a5e6
JJhwan7099 <105360636+JJhwan7099@users.noreply.github.com>
2023-08-15 00:38:25 +0900

    [Gold V] Title: 최소비용 구하기, Time: 140 ms, Memory: 8716 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+#define FOR(i,N) for(int i=1;i<=N;i++)
+int N, M;
+vector<int> v1[1001];
+int tot[1001];
+int cost[1001][1001];
+bool visit[1001][1001];
+bool zerocheck[1001][1001];
+void search(int start)
+{
+    queue<int> q1;
+    q1.push(start);
+    while(!q1.empty())
+    {
+        int a = q1.front();
+        for(int i = 0; i < v1[a].size(); i++)
+        {
+            if(visit[a][v1[a][i]] != 1)
+            {
+                q1.push(v1[a][i]);
+                visit[a][v1[a][i]] = 1;
+            }
+            if(tot[v1[a][i]] > tot[a] + cost[a][v1[a][i]] || tot[v1[a][i]] == -1)
+            {
+                if(tot[v1[a][i]] == -1) tot[v1[a][i]] = 0;
+                if(tot[a] == -1) tot[a] = 0;;
+                tot[v1[a][i]] = tot[a] + cost[a][v1[a][i]];
+                q1.push(v1[a][i]); 
+                /*이게 젤 중요했음 각 노드로 갈 수 있는 최단 거리가 계속
+                업데이트 되기 때문에, 업데이트 될 때 마다 그 노드를 기준으로
+                주변노드의 값들을 다시 업데이트 해줘야함!!!!!*/
+            }
+        }
+        q1.pop();
+    }
+}
+int main()
+{
+    cin >> N >> M;
+    FOR(i,M)
+    {
+        int a,b; cin >> a >> b;
+        v1[a].push_back(b);
+        int c; cin >> c;
+        if(zerocheck[a][b] == true)
+        {
+            continue;
+        }
+        else if(c == 0)
+        {
+            zerocheck[a][b] = 1;
+            cost[a][b] = 0;
+            continue;
+        }
+        else if(c < cost[a][b] || zerocheck[a][b] != 1)
+        {
+            if(zerocheck[a][b] != 1)
+            {
+                if(cost[a][b]==0)
+                    cost[a][b] = c;
+                else if(c < cost[a][b])
+                {
+                    cost[a][b] = c;
+                }
+            }
+        }
+    }
+    int start, end; cin >> start >> end;
+    fill(&tot[0],&tot[1001],-1);
+    search(start);
+    cout << tot[end];
+}
