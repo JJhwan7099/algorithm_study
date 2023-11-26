7d26f916ee85d70df346a7ad9595646ad37468e3
JJhwan7099 <105360636+JJhwan7099@users.noreply.github.com>
2023-11-26 20:44:04 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 252 ms, Memory: 9824 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -6,22 +6,14 @@ using namespace std;
 #define FOR(i,N) for(int i=1; i<=N; i++)
 int N, M, R;
 vector<int> v[100001];
-int main()
+int ans[100001] = {0,};
+int visit[100001];
+void bfs()
 {
-    cin >> N >> M >> R;
-    FOR(i,M)
-    {
-        int a, b; cin >> a >> b;
-        v[a].push_back(b);
-        v[b].push_back(a);
-    }
-    FOR(i,N) sort(v[i].begin(), v[i].end());
     int cnt = 1;
-    int visit[100001];
     queue<int> q;
     q.push(R);
     visit[R]=1;
-    int ans[100001] = {0,};
     while(q.size()>0)
     {
         int a = q.front();
@@ -36,6 +28,18 @@ int main()
         }
         q.pop();
     }
+}
+int main()
+{
+    cin >> N >> M >> R;
+    FOR(i,M)
+    {
+        int a, b; cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    FOR(i,N) sort(v[i].begin(), v[i].end());
+    bfs();
     FOR(i,N)
     {
         cout << ans[i] << '\n';
