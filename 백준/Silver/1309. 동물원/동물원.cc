ecce05907f34ef56609aac544127fb917c75180c
JJhwan7099 <105360636+JJhwan7099@users.noreply.github.com>
2023-11-26 21:24:04 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.cc
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -2,17 +2,14 @@
 using namespace std;
 int N;
 long long int DP[100001][3];
-long long int DPP[100001];
 int main()
 {
     cin >> N;
     DP[1][0] = 1; // 한마리도 안두는 경우
     DP[1][1] = 1; // 왼쪽에 한마리 두는 경우
     DP[1][2] = 1; // 오른쪽에 한마리 두는 경우
-    // DPP[1] = 3;
     for(int i=2; i<=N; i++)
     {
-        // DPP[i] = DPP[i-1] + 
         DP[i][0] = (DP[i-1][0] + DP[i-1][1] + DP[i-1][2])%9901;
         DP[i][1] = (DP[i-1][0] + DP[i-1][2])%9901;
         DP[i][2] = (DP[i-1][0] + DP[i-1][1])%9901;
