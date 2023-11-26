1afdbc0683f0937191e6561f6ac5fdb3796620f2
JJhwan7099 <105360636+JJhwan7099@users.noreply.github.com>
2023-11-26 20:46:37 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 252 ms, Memory: 11184 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int N, M, R;
+vector<int> v[100001];
+int visit[100001];
+int ans[100001];
+int cnt = 1;
+bool cmp(int&a, int&b)
+{
+    return a>b;
+}
+void dfs(int n)
+{
+    visit[n] = 1;
+    ans[n] = cnt++;
+    for(int i=0; i<v[n].size(); i++)
+    {
+        if(visit[v[n][i]]!=1) dfs(v[n][i]);
+    }
+}
+int main()
+{
+    cin >> N >> M >> R;
+    for(int i=0; i<M; i++)
+    {
+        int a,b; cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    for(int i=1; i<=N; i++)
+    {
+        sort(v[i].begin(), v[i].end(), cmp);
+    }
+    dfs(R);
+    for(int i=1; i<=N; i++)
+    {
+        cout << ans[i] << '\n';
+    }
+}
