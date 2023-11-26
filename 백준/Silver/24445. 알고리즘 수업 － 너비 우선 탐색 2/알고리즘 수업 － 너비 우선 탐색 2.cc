531df543030cc84cf11d38b13874d8a156357f24
JJhwan7099 <105360636+JJhwan7099@users.noreply.github.com>
2023-11-26 20:45:37 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 280 ms, Memory: 9096 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+#define FOR(i,N) for(int i=1; i<=N; i++)
+int N, M, R;
+vector<int> v[100001];
+int ans[100001] = {0,};
+int visit[100001];
+bool cmp(int& a, int& b)
+{
+    return a>b;
+}
+void bfs()
+{
+    int cnt = 1;
+    queue<int> q;
+    q.push(R);
+    visit[R]=1;
+    while(q.size()>0)
+    {
+        int a = q.front();
+        ans[a]= cnt++;
+        for(int i=0; i<v[a].size(); i++)
+        {
+            if(visit[v[a][i]]!=1)
+            {
+                visit[v[a][i]]=1;
+                q.push(v[a][i]);
+            }
+        }
+        q.pop();
+    }
+}
+int main()
+{
+    cin >> N >> M >> R;
+    FOR(i,M)
+    {
+        int a, b; cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    FOR(i,N) sort(v[i].begin(), v[i].end(), cmp);
+    bfs();
+    FOR(i,N)
+    {
+        cout << ans[i] << '\n';
+    }
+}
