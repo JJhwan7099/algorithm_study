40852490aebf4a653263c639e4d9538321d09087
JJhwan7099 <105360636+JJhwan7099@users.noreply.github.com>
2023-12-25 17:39:57 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -1,11 +1,8 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 using namespace std;
 vector<int> solution(vector<string> wallpaper) {
-    vector<int> answer;
     vector<pair<int,int>> v;
-    vector<pair<int,int>> v1;
     for(int i=0;i<wallpaper.size();i++)
     {
         for(int j=0;j<wallpaper[i].length();j++)
