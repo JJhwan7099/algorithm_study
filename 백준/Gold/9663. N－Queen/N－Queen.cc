25f69a561cbe735075d8aeb769dbb24abba35735
JJhwan7099 <105360636+JJhwan7099@users.noreply.github.com>
2024-02-20 00:37:38 +0900

    [Gold IV] Title: N-Queen, Time: 8036 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -3,7 +3,6 @@ using namespace std;
 #define FOR(i,N) for(int i=1; i<=N; i++)
 int N;
 int graph[16][16];
-int visit[16][16];
 int ans = 0;
 int dx[3] = {-1,-1,-1};
 int dy[3] = {-1,0,1};
@@ -32,12 +31,10 @@ void backtracking(int cnt)
     }
     FOR(j,N)
     {
-        if(visit[cnt][j]!=1 && check(cnt,j))
+        if(graph[cnt][j]==0 && check(cnt,j))
         {
-            visit[cnt][j]=1;
             graph[cnt][j]=1;
             backtracking(cnt+1);
-            visit[cnt][j]=0;
             graph[cnt][j]=0;
         }
     }
